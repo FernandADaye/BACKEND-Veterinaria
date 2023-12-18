ed3120c31c0f5231732ac80b0b6a2b6a9442d556
Fernanda López <fer.daye.2007@gmail.com>
2023-12-18 17:30:35 -0600

    Aray simulando ser una BD

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
-// esto es para express 
-const express = require('express');
+// esto es para express
+const express = require("express");
 const app = express();
 
-// definir que puerto es 
+// definir que puerto es
 const PORT = 3000;
 
-// es necesario hacer una prueva para que se vea reflejado en el servidor 
-app.get('/', (req, res) => {
-    console.log('req', req);
-    res.send('ya funciona o no?');
+// es necesario hacer una prueva para que se vea reflejado en el servidor
+app.get("/", (req, res) => {
+  console.log("req", req);
+  res.send("ya funciona o no?");
 });
 
+const idDueños = [
+  { id: 1, nombre: "Dayé", mascota: "gato" },
+  { id: 2, nombre: "Azul ", mascota: "ratón" },
+  { id: 3, nombre: "Erika ", mascota: "perro" },
+  { id: 4, nombre: "Leyla", mascota: "mapache " },
+  { id: 5, nombre: "Levi ", mascota: "conejo " },
+];
 
-
-
-
-
-
-// indica donde será el puerto, la funcion menciona que es lo que se hara cuando se inice el servidor 
+// indica donde será el puerto, la funcion menciona que es lo que se hara cuando se inice el servidor
 app.listen(PORT, () => {
-    console.log(`Jaló en el puerto ${PORT}`);
+  console.log(`Jaló en el puerto ${PORT}`);
 });
