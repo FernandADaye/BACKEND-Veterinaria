e9c517853027314d51936fb38122b9af75bc0930
Fernanda López <fer.daye.2007@gmail.com>
2023-12-18 16:40:53 -0600

    no funciona

--- src/queryPruevas.js
+++ src/queryPruevas.js
@@ -1,6 +1,8 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+// definir que puerto es 
+const port = 3000;
 // indica donde será el puerto, la funcion menciona que es lo que se hara cuendo se inice el servidor 
-app.listen("PORT", () => {
+app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`);
 });
