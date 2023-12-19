52f08b72c8bd69eae991049d47be2e3dad2b69fb
Fernanda López <fer.daye.2007@gmail.com>
2023-12-18 20:17:12 -0600

    ejustes en endpoints en nuevos lugares junto con la bd

---
+++ src/servicios/idDueños/index.js
@@ -0,0 +1,45 @@
+// aqui es donde comienza la estructura buena 
+class idDueños {
+    constructor () {
+
+        this.idDueños = []
+        this.generadorData = ()
+        
+    }
+    //❌⭕❌⭕❌⭕❌⭕❌⭕❌⭕ es como si aquí estubierá la BD (sigamos fingiendo )❌⭕❌⭕❌⭕❌⭕❌⭕❌⭕❌⭕❌⭕
+    generadorData () {
+            this.idDueños = [
+                { id: 1, nombre: "Dayé", mascota: "gato" },
+                { id: 2, nombre: "Azul ", mascota: "ratón" },
+                { id: 3, nombre: "Erika ", mascota: "perro" },
+                { id: 4, nombre: "Leyla", mascota: "mapache " },
+                { id: 5, nombre: "Levi ", mascota: "conejo " },
+            ]
+        }
+
+    // aqui se encontraran todos los endpoints que se usarán  (un poco diferentes, pero hacen lo mismmo )
+    //💜 get 
+        getDueños (){
+            return this.idDueños
+        }
+
+    //💜 post
+        nuevaInfo (){
+            this.idDueños.push(nuevaInfo);
+        }
+
+    //💜 patch  
+        idModificado (idModificado){
+            const posicion = this.idDueños.findIndex(idDueño=> idDueño.id===parseInt(idModificado));
+            this.idDueños[posicion] = { ...this.idDueños[posicion], ...nuevoDato };
+        }
+
+    //💜 delete
+        idBorrado (idBorrado){
+            const posicion = this.idDueños.findIndex(idDueño=> idDueño.id===parseInt(idBorrado));
+            this.idDueños.splice(posicion, 1);
+        }
+
+    }
+
+    module.exports = { idDueños }
