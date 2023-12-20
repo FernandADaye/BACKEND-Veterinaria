888657a7259ad3eb1a1994de1ad83437d4456f49
Fernanda López <fer.daye.2007@gmail.com>
2023-12-19 20:24:26 -0600

    creando las rutas

--- src/routes/idDueños/index.js
+++ src/routes/idDueños/index.js
@@ -1,2 +1,14 @@
 const express = require('express')
-express.Router();
+const dueñosRutas = express.Router()
+
+const{
+    traer,
+    crear,
+    editarInfo,
+    eliminar
+}= require('../../controllers/idDueños/index')
+
+dueñosRutas.get('/', traer)
+dueñosRutas.post('/', crear )
+dueñosRutas.patch('/:id', editarInfo )
+dueñosRutas.delete('/:id', eliminar )
