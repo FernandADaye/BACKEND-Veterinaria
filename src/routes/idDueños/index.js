2734ce0eb532690e3dfba3b75a9819bda175ac3a
Fernanda López <fer.daye.2007@gmail.com>
2023-12-19 21:24:41 -0600

    más documentacion

--- src/routes/idDueños/index.js
+++ src/routes/idDueños/index.js
@@ -1,6 +1,8 @@
+// uso de express
 const express = require('express')
 const dueñosRutas = express.Router()
 
+// aqui es de donde se enlaza con controllers (get, patch, post, delete)
 const{
     traer,
     crear,
@@ -8,9 +10,11 @@ const{
     eliminar
 }= require('../../controllers/idDueños/index')
 
+//estas como tal son las rutas para cada funcion de endpoint
 dueñosRutas.get('/', traer)
 dueñosRutas.post('/', crear )
 dueñosRutas.patch('/:id', editarInfo )
 dueñosRutas.delete('/:id', eliminar )
 
+//obvio que es para exportar 
 module.exports = {dueñosRutas};
--- src/routes/idDueños/main.routers.js
+++ src/routes/idDueños/main.routers.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const { dueñosRutas } = require("../idDueños/index");
-
+//se enlaza con el servidor y las rutas 
 const routerApi = (app) => {
     app.use("", dueñosRutas);
 };
 
-
+//obvio que es para exportar 
 module.exports = { routerApi };
