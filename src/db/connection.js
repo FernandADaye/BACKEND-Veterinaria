7cf4d3d6c746a58e5dedac83b84a59ebc100deaf
Fernanda López <fer.daye.2007@gmail.com>
2023-12-21 13:29:21 -0600

    inicio para comenzar a hacer la conexion

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,5 +1,6 @@
 const { Pool} = require ('pg')
-const {config } = require ('../config/index')
+const {config } = require ('../config/index');
+const { text } = require('express');
 
 // hacer una destructuracion de config, se simplifica 
 const {
@@ -17,3 +18,8 @@ new Pool ({
     port:portDB ,
     database:nameDB
 })
+
+module.exports = {
+    query: (queryStrings, Arrayparams) =>Pool.query(queryStrings, Arrayparams),
+    close: () => Pool.end()
+}
